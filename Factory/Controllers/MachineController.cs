979f7cefd4444d38d1c0ab26ef7564e0b9f2ee0b
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-01-07 13:53:13 -0800

    add single delete functionality to Machine via remove engineer route

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -30,5 +30,41 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Detail(int id)
+    {
+      Machine target = _db.Machines
+        .Include(m=>m.LicensedEngineers)
+        .ThenInclude(join=>join.Engineer)
+        .FirstOrDefault(m=>m.MachineId==id);
+      return View(target);
+    }
+    public ActionResult Edit(int id)
+    {
+      Machine target = _db.Machines
+        .Include(m=>m.LicensedEngineers)
+        .ThenInclude(join=>join.Engineer)
+        .FirstOrDefault(m=>m.MachineId==id);
+      ViewBag.SelectEngineer = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
+      return View(target);
+    }
+    [HttpPost]
+    public ActionResult Edit(Machine m, int SelectEngineer)
+    {
+      _db.Entry(m).State = EntityState.Modified;
+      _db.SaveChanges();
+      if(SelectEngineer != 0)
+      {
+        _db.EngineerMachines.Add(new EngineerMachine(){EngineerId = SelectEngineer, MachineId = m.MachineId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Detail",new {id = m.MachineId});
+    }
+    public ActionResult RemoveEngineer(int id)
+    {
+      EngineerMachine target = _db.EngineerMachines.FirstOrDefault(em=>em.EngineerMachineId==id);
+      _db.Remove(target);
+      _db.SaveChanges();
+      return RedirectToAction("Detail", new{id=target.MachineId});
+    }
   }
 }
