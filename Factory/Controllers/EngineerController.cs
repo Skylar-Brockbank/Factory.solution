0de92ec502d468ae21854df9d9f5f35c9b1e9205
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-01-07 10:38:50 -0800

    add rough views and fix git issues

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Factory.Models;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class EngineerController : Controller
   {
+    private readonly skylar_brockbankContext _db;
+    public EngineerController(skylar_brockbankContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      ViewBag.EngineerList = _db.Engineers.ToList();
       return View();
     }
   }
