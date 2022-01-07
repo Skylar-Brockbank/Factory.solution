d4939a8202c49abf6767763a9ef4f9788f4fb43c
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-01-07 12:04:36 -0800

    rough in edit routes

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Factory.Models;
 using System.Linq;
 
@@ -36,6 +37,29 @@ namespace Factory.Controllers
         .ThenInclude(join => join.Machine)
         .FirstOrDefault(engineer => engineer.EngineerId == id);
       return View(target);
+    }
+    public ActionResult Edit(int id)
+    {
+      //get the specified engineer and prepare a select list for adding machines
+      //you're gonna add a textbox for name so that you can change the name property
+      Engineer target = _db.Engineers
+        .Include(engineer => engineer.Machines)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.SelectMachines = new SelectList(_db.Machines,"MachineId", "DeviceName");
+      return View(target);
+    }
+    [HttpPost]
+    public ActionResult Edit(Engineer e, int SelectMachines)
+    {
+      _db.Entry(e).State = EntityState.Modified;
+      _db.SaveChanges();
+      if(SelectMachines != 0)
+      {
+        _db.EngineerMachines.Add(new EngineerMachine() {EngineerId = e.EngineerId, MachineId = SelectMachines});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Detail", new{id = e.EngineerId});
     }
   }
 }
